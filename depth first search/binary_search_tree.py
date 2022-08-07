1ac8a495f149f84a9d4c39587df4673eb3c55ab0
Max Boykin <89083313+maxxboykin@users.noreply.github.com>
2022-08-07 19:37:58 -0400

    doing some BST

---
+++ depth first search/binary_search_tree.py
@@ -0,0 +1,11 @@
+
+
+# a BST is a rooted binary tree with a value of each internal node being greater than all values
+# in the respective node's left subtree and less than ones in its right
+# subtree 
+
+# primary purpose of a BST is to search like a binary search
+
+# the time complexity of each search is O(h) is the height of the tree
+
+
